c4e0292b67be2c17eed7ce75b201d683631cba6b
Venkatesh Yadav <golla.Venkatesh.yadav@gmail.com>
2020-11-26 20:31:30 -0500

    D-02 Writting Patterns

--- src/com/codingBlocks/Basics/patterns/Pattern4.java
+++ src/com/codingBlocks/Basics/patterns/Pattern4.java
@@ -1,2 +1,36 @@
-package com.codingBlocks.Basics.patterns;public class Pattern4 {
+package com.codingBlocks.Basics.patterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int numberOfSpaces = n - 1;
+        int numberOfStars = 1;
+        // rows
+        int rows = 1;
+        while (rows <= n) {
+
+            // work for spaces
+            int countOfSpaces = 1;
+            while (countOfSpaces <= numberOfSpaces) {
+                System.out.print(" \t");
+                countOfSpaces++;
+            }
+
+            // work for stars
+            int countOfStars = 1;
+            while (countOfStars <= numberOfStars) {
+                System.out.print("*\t");
+                countOfStars++;
+            }
+
+            System.out.println();
+            numberOfStars++;
+            numberOfSpaces--;
+            rows++;
+        }
+    }
 }
