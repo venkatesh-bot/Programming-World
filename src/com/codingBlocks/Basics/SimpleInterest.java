1958b59c6716848443d7462167595d797fb2ae8d
Venkatesh Yadav <golla.Venkatesh.yadav@gmail.com>
2020-12-05 01:41:32 -0500

    Day-AfterLong Time : variable naming convention fix

--- src/com/codingBlocks/Basics/SimpleInterest.java
+++ src/com/codingBlocks/Basics/SimpleInterest.java
@@ -4,10 +4,10 @@ public class SimpleInterest {
 
     public static void main(String[] args) {
         int principal = 1000;
-        int rat = 5;
+        int rate = 5;
         int time = 2; // In years
 
-        int simpleInterest = (principal * rat * time) / 100;
+        int simpleInterest = (principal * rate * time) / 100;
 
         System.out.println("Simple Interest is " + simpleInterest);
     }
