b7964adb983802fea6b0c44479945ebd7b426bf1
Venkatesh Yadav <golla.Venkatesh.yadav@gmail.com>
2020-12-12 01:23:35 -0500

    Amex-Cognizant

---
+++ src/com/interview/Student.java
@@ -0,0 +1,35 @@
+package com.interview;
+
+public class Student {
+    String firstName;
+    String LastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.LastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                '}';
+    }
+}
